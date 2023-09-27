d76aab3bd82cb0816706abcca78189a66e82ab07
tizkovatereza <135881365+tizkovatereza@users.noreply.github.com>
2023-09-27 15:30:04 +0200

    Update Basics.js

--- Basics.js
+++ Basics.js
@@ -1,1 +1,21 @@
 
+// Printing
+
+
+// Operators can work for strings as well.
+// This process of appending one string to another is called concatenation.
+
+console.log('hi' + 'ya'); // Prints 'hiya'
+console.log('wo' + 'ah'); // Prints 'woah'
+console.log('I love to ' + 'code.')
+// Prints 'I love to code.'
+console.log('front ' + 'space'); 
+// Prints 'front space'
+console.log('back' + ' space'); 
+// Prints 'back space'
+console.log('no' + 'space'); 
+// Prints 'nospace'
+console.log('middle' + ' ' + 'space'); 
+// Prints 'middle space'
+
+console.log("Hello" + "World")
