510f31dffcb575763e3f107f831a5596ec07d545
tizkovatereza <135881365+tizkovatereza@users.noreply.github.com>
2023-10-02 23:57:06 +0200

    Update Basics.js

--- Basics.js
+++ Basics.js
@@ -1,6 +1,5 @@
 
-// Printing
-
+// PRINTING
 
 // Operators can work for strings as well.
 // This process of appending one string to another is called concatenation.
@@ -19,3 +18,40 @@ console.log('middle' + ' ' + 'space');
 // Prints 'middle space'
 
 console.log("Hello" + "World")
+
+
+// DATA TYPES
+
+// We work with instances of data types (e.g. "Hello" is an instance of a string data type)
+// We can apply different methods or find out different things about different data types
+// Example: "Hello".length gives us length of a word "Hello"
+
+
+// METHODS
+
+// Example: console.log() is using log method on console object
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy');
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+console.log("Codecademy".toUpperCase()) // Be careful about the capital letters !!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
