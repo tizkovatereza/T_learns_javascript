263433ee91e94daea875ed57e40873aa2c0f6c78
tizkovatereza <135881365+tizkovatereza@users.noreply.github.com>
2023-10-03 00:31:29 +0200

    Update Basics.js

--- Basics.js
+++ Basics.js
@@ -39,6 +39,40 @@ console.log('    Remove whitespace   '.trim());
 console.log("Codecademy".toUpperCase()) // Be careful about the capital letters !!!
 
 
+// BUILT-IN OBJECTS
+
+console.log(Math.random());
+// Prints a random number between 0 and 1
+
+Math.random()*50;
+
+//You can use Math.floor() to round down a random number like this:
+Math.floor(Math.random()*50);
+
+console.log(Math.floor(Math.random()*50));
+// Math.floor() takes a decimal number, and rounds down to the nearest whole number
+// In this case:
+// Math.random() generates a random number between 0 and 1.
+// We then multiply that number by 50, so now we have a number between 0 and 50.
+// Then, Math.floor() rounds the number down to the nearest whole number.
+
+console.log(Math.ceil(43.8));   // Returns the smallest integer greater than or equal to a decimal number
+console.log(Number.isInteger(2017)); 
+
+
+// Let’s take one more glance at the concepts we just learned:
+
+// Data is printed, or logged, to the console, a panel that displays messages, with console.log().
+// We can write single-line comments with // and multi-line comments between /* and */.
+// There are 7 fundamental data types in JavaScript: strings, numbers, booleans, null, undefined, symbol, and object.
+// Numbers are any number without quotes: 23.8879
+// Strings are characters wrapped in single or double quotes: 'Sample String'
+// The built-in arithmetic operators include +, -, *, /, and %.
+// Objects, including instances of data types, can have properties, stored information. The properties are denoted with a . after the name of the object, for example: 'Hello'.length.
+// Objects, including instances of data types, can have methods which perform actions. Methods are called by appending the object or instance with a period, the method name, and parentheses. For example: 'hello'.toUpperCase().
+// We can access properties and methods by using the ., dot operator.
+// Built-in objects, including Math, are collections of methods and properties that JavaScript provides.
+
 
 
 
