5bcdd443f9b4b3e8169dc86c90cfe70337435221
tizkovatereza <135881365+tizkovatereza@users.noreply.github.com>
2023-10-25 22:29:35 -0700

    Update Basics.js

--- Basics.js
+++ Basics.js
@@ -407,7 +407,18 @@ const logSkyColor = () => {
 console.log(color); // throws a ReferenceError
 
 
+// In this lesson, you learned about scope and how it impacts the accessibility of different variables.
 
+// Let’s review the following terms:
+
+// Scope refers to where variables can be accessed throughout the program, and is determined by where and how they are declared.
+// Blocks are statements that exist within curly braces {}.
+// Global scope refers to the context within which variables are accessible to every part of the program.
+// Global variables are variables that exist within global scope.
+// Block scope refers to the context within which variables are accessible only within the block they are defined.
+// Local variables are variables that exist within block scope.
+// Global namespace is the space in our code that contains globally scoped information.
+// Scope pollution is when too many variables exist in a namespace or variable names are reused.
 
 
 
