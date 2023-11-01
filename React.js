72d5411e7a2ddff9f9ad0e7a1b72866f174526b5
tizkovatereza <135881365+tizkovatereza@users.noreply.github.com>
2023-11-01 14:34:24 +0100

    Update React.js

--- React.js
+++ React.js
@@ -46,14 +46,50 @@ const myTeam = {
 
 const myArticle = <article> </article>;
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// JSX ATTRIBUTES
+
+// JSX elements can have attributes, just like how HTML elements can.
+// A JSX attribute is written using HTML-like syntax: a name, followed by an equals sign, followed by a value.
+
+//The value should be wrapped in quotes, like this:
+tereza-attribute-name = "some-name"
+
+// Here are some JSX elements with attributes:
+
+<a href='http://www.example.com'>Welcome to the Web</a>;
+const title = <h1 id='title'>Introduction to React.js: Part I</h1>; 
+
+// A single JSX element can have many attributes, just like in HTML:
+const panda = <img src='images/panda.jpg' alt='panda' width='500px' height='500px'>;
+
+// Examples:
+const p1 = <p id='large'> foo </p>;
+const p2 = <p id='small'> bar </p>;
+
+// There are no quotation marks for the expression in the elements
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// NESTED JSX
+
+<a href="https://www.example.com"><h1>Click me!</h1></a>
+
+// Nicer format:
+<a href="https://www.example.com">
+  <h1>
+    Click me!
+  </h1>
+</a>
 
 
-
-
-
-
-
-
+// If we want multi-line -> use parentheses
+(
+  <a href="https://www.example.com">
+    <h1>
+      Click me!
+    </h1>
+  </a>
+)
 
 
 
