1ecf300499f75082eada95709536eafdf203e287
tizkovatereza <135881365+tizkovatereza@users.noreply.github.com>
2023-11-01 16:06:25 +0100

    Update React.js

--- React.js
+++ React.js
@@ -115,6 +115,18 @@ const paragraphs = (
 );
 
 
+// Div element - solution for wrapping your JSX expression if it contains multiple outer elements. 
+const blog = (
+  <div>
+  <img src="pics/192940u73.jpg" />
+  <h1>
+    Welcome to Dan's Blog!
+  </h1>
+  <article>
+    Wow I had the tastiest sandwich today. I <strong>literally</strong> almost freaked out.
+  </article>
+  </div>
+);
 
 
 
