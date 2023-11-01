1a7dacd94ceb6a184e52bcc5282bb0ac075a1b26
tizkovatereza <135881365+tizkovatereza@users.noreply.github.com>
2023-11-01 13:54:14 +0100

    Update React.js

--- React.js
+++ React.js
@@ -1,1 +1,15 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// INTRO
 
+// Take a look at the following line of code:
+
+const h1 = <h1> Hello world </h1>;
+
+// However, the code also contains <h1>Hello world</h1>, which looks exactly like HTML. That part wouldn’t work if you tried to run it in a JavaScript file.
+// Despite what it looks like, your code doesn’t actually contain any HTML at all.
+
+// The part that looks like HTML, <h1>Hello world</h1>, is something called JSX.
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
