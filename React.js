2bc9177904b9f3b095b0171e0b016d04ae03c0f8
tizkovatereza <135881365+tizkovatereza@users.noreply.github.com>
2023-11-01 13:55:35 +0100

    Update React.js

--- React.js
+++ React.js
@@ -12,4 +12,11 @@ const h1 = <h1> Hello world </h1>;
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// 
+// JSX
+
+// JSX is a syntax extension for JavaScript. It was written to be used with React. JSX code looks a lot like HTML.
+// The “syntax extension” means that JSX is not valid JavaScript. Web browsers can’t read it!
+
+// If a JavaScript file contains JSX code, then that file will have to be compiled.
+// This means that before the file reaches a web browser, a JSX compiler will translate any JSX into regular JavaScript.
+
